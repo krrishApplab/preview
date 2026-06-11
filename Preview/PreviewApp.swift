bc05de176e0ab2a4903c4e9740a9dbc68fd29ea6
Krishna Raj S <krishnarajsalim@gmail.com>
2026-06-11 11:14:33 +0530

    initial commit*

---
+++ Preview/PreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewApp.swift
+//  Preview
+//
+//  Created by Krishna Raj on 11/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
