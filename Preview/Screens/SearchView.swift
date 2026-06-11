fdbc34475aa5b29c3991fdd2c4815d3c96c64d94
Krishna Raj S <krishnarajsalim@gmail.com>
2026-06-11 12:00:58 +0530

    Search text change to -> Favourite Items**

--- Preview/Screens/SearchView.swift
+++ Preview/Screens/SearchView.swift
@@ -12,7 +12,7 @@ struct SearchView: View {
     var body: some View {
         NavigationStack {
             VStack(spacing: 12) {
-                Text("Search")
+                Text("Favourite Items**")
                     .font(.largeTitle.bold())
 
                 Text("Find what you're looking for.")
